0e562ad143a2aa9835d710cb4c100642f2e118e9
Xukay101 <zaca03zaca@gmail.com>
2023-12-10 11:52:58 -0300

    [Feature] Implement likes functionality

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 from time import sleep
+from random import random
 
 import undetected_chromedriver as uc
 from selenium.webdriver.support import expected_conditions as EC
@@ -18,8 +19,9 @@ class TinderBot:
         self.driver.get(self.tinder_url)
         self.allow_location()
         self.login()
+        self.give_likes()
 
-        sleep(20)
+        sleep(100)
 
     def login(self):
         tinder_login_button = self.driver.find_element(By.XPATH, settings.XPATH_TINDER_LOGIN_BUTTON)
@@ -47,7 +49,6 @@ class TinderBot:
         password_input.send_keys(Keys.ENTER)
 
         self.driver.switch_to.window(main_page)
-
         
     def allow_location(self):
         self.driver.execute_cdp_cmd(
@@ -60,11 +61,52 @@ class TinderBot:
         self.driver.execute_cdp_cmd(
             "Emulation.setGeolocationOverride",
             {
-                "latitude": -34.603722,
-                "longitude": -58.381592,
+                "latitude": settings.LATITUDE,
+                "longitude": settings.LONGITUDE,
                 "accuracy": 100,
             },
         )
 
+    def give_likes(self):
+        html_element = self.driver.find_element(By.TAG_NAME, 'html')
+        while True:
+            # Browse photos
+            self.browse_photos()
+            sleep(1)
+
+            # Probability of like
+            if random() < settings.LIKE_PROBABILITY:
+                # like_button = self.driver.find_element(By.XPATH, settings.XPATH_TINDER_LIKE_BUTTON)
+                # like_button.click()
+                html_element.send_keys(Keys.ARROW_RIGHT)
+
+                # Ignore if matching
+                # if self.is_math():
+                #     ignore_button = self.driver.find_element(By.XPATH, settings.XPATH_TINDER_MATH_IGNORE_BUTTON)
+            else:
+                # deny_button = self.driver.find_element(By.XPATH, settings.XPATH_TINDER_DENY_BUTTON)
+                # deny_button.click()
+                html_element.send_keys(Keys.ARROW_LEFT)
+
+
+    def browse_photos(self):
+        photos_container = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, settings.XPATH_TINDER_PHOTOS_CONTAINER)))
+        # REFACTORIZAR
+        html_element = self.driver.find_element(By.TAG_NAME, 'html')
+        for _ in range(6):
+            html_element.send_keys(Keys.SPACE)
+            sleep(1)
+
+        # next_photo_button = self.driver.find_element(By.XPATH, settings.XPATH_TINDER_NEXT_PHOTO_BUTTON)
+        # for _ in range(len(photos_elements)-1):
+        #     next_photo_button.click()
+
+    def is_math(self):
+        try:
+            math_element = WebDriverWait(self.driver, 1).until(EC.presence_of_element_located((By.XPATH, settings.XPATH_TINDER_MATH_CONTAINER)))
+            return True
+        except:
+            return False
+        
     def close(self):
         self.driver.quit()
--- src/config.py
+++ src/config.py
@@ -13,16 +13,26 @@ class Settings(BaseSettings):
     TINDER_URL: str  = 'https://tinder.com'    
     GOOGLE_EMAIL: str
     GOOGLE_PASSWORD: str
+    LIKE_PROBABILITY: float = 0.8
 
     # Location
     LATITUDE: float = -34.603722
     LONGITUDE: float = -58.381592
 
     # XPath configurations
-    XPATH_TINDER_LOGIN_BUTTON: str = '//*[@id="q488047873"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a'
     XPATH_GOOGLE_LOGIN_BUTTON: str = '//*[@id="q1569882032"]'
     XPATH_GOOGLE_EMAIL_INPUT: str = '//*[@id="identifierId"]'
     XPATH_GOOGLE_PASSWORD_INPUT: str = '//*[@id="password"]/div[1]/div/div[1]/input'
+    XPATH_TINDER_LOGIN_BUTTON: str = '//*[@id="q488047873"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a'
     XPATH_TINDER_ALLOW_LOCATION_BUTTON: str = '//*[@id="q-1240333203"]/main/div/div/div/div[3]/button[1]/div[2]/div[2]'
+    XPATH_TINDER_PHOTOS_CONTAINER: str = '//*[@id="q488047873"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div[1]/span[1]'
+    XPATH_TINDER_NEXT_PHOTO_BUTTON: str = '//*[@id="q488047873"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div[1]/span[3]/div'
+    XPATH_TINDER_LIKE_BUTTON: str = '//*[@id="q488047873"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div/div[4]/button'
+    XPATH_TINDER_DENY_BUTTON: str = '//*[@id="q488047873"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div/div[2]/button'
+    XPATH_TINDER_MATH_CONTAINER: str = ''
+    XPATH_TINDER_MATH_IGNORE_BUTTON: str = ''
+
+
+    
 
 settings = Settings()
